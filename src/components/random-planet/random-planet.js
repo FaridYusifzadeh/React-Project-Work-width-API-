7af0b1e3c5d0bc96330feb7da3d9d7f291556a6a
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-01-13 18:49:00 +0400

    add loading spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,55 +1,72 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
+import Spinner from '../spinner';
 
 export default class RandomPlanet extends Component {
   SwapiService = new SwapiService();
   constructor() {
     super();
     this.state = {
-      planet: {}
+      planet: {},
+      loading: true
     };
     this.updatePlanet();
   }
 
-omPlanetLoaded = (planet) => {
-this.setState({planet})
-}
- 
+  omPlanetLoaded = planet => {
+    this.setState({ planet, loading: false });
+  };
 
   updatePlanet() {
     const id = 12;
     this.SwapiService.getPlanet(id).then(this.omPlanetLoaded);
-    };
-  
+  }
+
   render() {
-    const {planet: { id, name, population, rotationPeriod, diameter} } = this.state;
+    const { planet, loading } = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
+
+    // if (loading) {
+    //   return <Spinner />;
+    // }
 
     return (
       <div className='random-planet jumbotron rounded'>
-        <img
-          className='planet-image'
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          alt='planet'
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <span className='term'>Population</span>
-              <span>{population}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {spinner} {content}
       </div>
     );
   }
 }
+
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img
+        className='planet-image'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt='planet'
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <span className='term'>Population</span>
+            <span>{population}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -5,7 +5,7 @@ export default class SwapiService {
     const res = await fetch(`${this._apiBase}${url}`);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` `, received ${res.status}`);
+      throw new Error(`Could not fetch ${url}``, received ${res.status}`);
     }
     return await res.json();
   }

ab0d0ea47f9cdd5408525cdad1b85ea938fd9d86
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-01-13 14:51:30 +0400

    creating components

--- src/index.js
+++ src/index.js
@@ -1,50 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.co/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}``, received ${res.status}`);
-    }
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then(people => {
-  people.forEach(planet => {
-    //   console.log(planet.name);
-  });
-});
-
-swapi.getStarship(3).then(planet => {
-  console.log(planet.name);
-});
+ReactDOM.render(<App />,
+  document.getElementById('root'));
